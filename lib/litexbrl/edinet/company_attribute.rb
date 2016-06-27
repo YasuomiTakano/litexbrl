d288754f2e2fe0f47ae1c4a5e346cc0be337e0e8
YasuomiTakano <yasuomi.takano@speee.jp>
2016-06-27 21:34:01 +0900

    company_attribute.rb内の処理記載(一旦tdnetのcompany_attribute.rbをコピー)

---
+++ lib/litexbrl/edinet/company_attribute.rb
@@ -0,0 +1,16 @@
+module LiteXBRL
+  module EDINET
+    module CompanyAttribute
+      attr_accessor :code, :company_name, :consolidation
+
+      def attributes_company
+        {
+          code: code,
+          company_name: company_name,
+          consolidation: consolidation,
+        }
+      end
+
+    end
+  end
+end
