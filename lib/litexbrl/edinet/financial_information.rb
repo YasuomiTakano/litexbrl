4ad70059f306696282eb8c80bffe39bfbd08b570
YasuomiTakano <yasuomi.takano@speee.jp>
2016-07-20 09:01:37 +0900

    取得できなかった情報の取得処理追記

--- lib/litexbrl/edinet/financial_information.rb
+++ lib/litexbrl/edinet/financial_information.rb
@@ -132,9 +132,9 @@ module LiteXBRL
         # 有価証券報告書の勘定科目の値を取得します
         #
         def find_value_jp_cor(doc, item, context, context_consolidation)
-
+          # puts "context_consolidation : #{context_consolidation}"
           find_value(doc, item, context) do |item, context|
-            "//xbrli:xbrl/jpcrp_cor:#{item}[@contextRef='#{context}' or @contextRef='#{context}_#{context_consolidation}'] | //xbrli:xbrl/jppfs_cor:#{item}[@contextRef='#{context}' or @contextRef='#{context}_#{context_consolidation}'] | //xbrli:xbrl/jpdei_cor:#{item}[@contextRef='#{context}' or @contextRef='#{context}_#{context_consolidation}']"
+            "//xbrli:xbrl/jpcrp_cor:#{item}[@contextRef='#{context}' or @contextRef='#{context}_Consolidated' or @contextRef='#{context}_NonConsolidatedMember'] | //xbrli:xbrl/jppfs_cor:#{item}[@contextRef='#{context}' or @contextRef='#{context}_Consolidated' or @contextRef='#{context}_NonConsolidatedMember'] | //xbrli:xbrl/jpdei_cor:#{item}[@contextRef='#{context}' or @contextRef='#{context}_Consolidated' or @contextRef='#{context}_NonConsolidatedMember']"
           end
         end
 
