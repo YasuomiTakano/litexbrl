073231e9d008f226b497b78d10953b0c6c2511ae
YasuomiTakano <yasuomi.takano@speee.jp>
2016-10-13 21:26:54 +0900

    XBRLファイルの編集

--- spec/edinet/financial_information_spec.rb
+++ spec/edinet/financial_information_spec.rb
@@ -13,7 +13,7 @@ module LiteXBRL
           context 'japan accounting standards' do
             let(:namespaces_array){['link', 'jpdei_cor', 'xbrldi', 'jpcrp040300-q2r_E05041-000', 'xlink', 'jpcrp_cor', 'xbrli', 'jppfs_cor', 'iso4217', 'xsi']}
             example 'when you get the name space' do
-              expect(FinancialInformation.send(:find_namespaces, doc("#{dir}/9449_q.xbrl"))).to match_array namespaces_array
+              expect(FinancialInformation.send(:find_namespaces, doc("#{dir}/quarter.xbrl"))).to match_array namespaces_array
             end
           end
         end
@@ -25,7 +25,7 @@ module LiteXBRL
             let(:context){'CurrentYTDDuration'}
             let(:context_consolidation){'Consolidated'}
             example 'when you get the net sales' do
-              expect(FinancialInformation.send(:find_value_jp_cor, doc("#{dir}/9449_q.xbrl"), item, context, context_consolidation)).to eq '66707000000'
+              expect(FinancialInformation.send(:find_value_jp_cor, doc("#{dir}/quarter.xbrl"), item, context, context_consolidation)).to eq '66707000000'
             end
           end
         end
@@ -38,7 +38,7 @@ module LiteXBRL
             let(:context){'CurrentYTDDuration'}
             let(:context_consolidation){'Consolidated'}
             example 'when you get a segment of net sales' do
-              expect(FinancialInformation.send(:find_value_jp_cor_segment, doc("#{dir}/9449_q.xbrl"), item, context_ref_name, context, context_consolidation)).to eq '31206000000'
+              expect(FinancialInformation.send(:find_value_jp_cor_segment, doc("#{dir}/quarter.xbrl"), item, context_ref_name, context, context_consolidation)).to eq '31206000000'
             end
           end
         end
@@ -48,7 +48,7 @@ module LiteXBRL
           context 'japan accounting standards' do
             let(:xpath_array){["//xbrli:xbrl/jpcrp_cor:NetSalesSummaryOfBusinessResults[@contextRef='CurrentYTDDuration']"]}
             example 'when you get the net sales by specifying the XPath' do
-              expect(FinancialInformation.send(:find_value, xpath_array, doc("#{dir}/9449_q.xbrl"))).to eq '66707000000'
+              expect(FinancialInformation.send(:find_value, xpath_array, doc("#{dir}/quarter.xbrl"))).to eq '66707000000'
             end
           end
         end
@@ -65,7 +65,7 @@ module LiteXBRL
             let(:id){"[starts-with(@id,'CurrentYTDDuration_') and not(substring-after(@id, 'ReportableSegmentsMember')) and (contains(@id, '-asr_') or contains(@id, '-q1r_') or contains(@id, '-q2r_') or contains(@id, '-q3r_'))]"}
 
             example 'when you get the OperatingSegmentsAxis' do
-              expect(FinancialInformation.send(:find_value_reportable_segments_member, doc("#{dir}/9449_q.xbrl"), id).map(&:to_s)).to eq result_array
+              expect(FinancialInformation.send(:find_value_reportable_segments_member, doc("#{dir}/quarter.xbrl"), id).map(&:to_s)).to eq result_array
             end
           end
         end
--- spec/edinet/security_report_spec.rb
+++ spec/edinet/security_report_spec.rb
@@ -10,10 +10,10 @@ module LiteXBRL
       describe ".read doc" do
         context '日本会計基準' do
           context "四半期報告書" do
-            let(:xbrl) { SecurityReport.read doc("#{dir}/9449_q.xbrl") }
+            let(:xbrl) { SecurityReport.read doc("#{dir}/quarter.xbrl") }
             let(:security_report) { xbrl[:security_report] }
             it do
-              expect(security_report[:code]).to eq '9449'
+              expect(security_report[:code]).to eq '1111'
               expect(security_report[:year]).to eq 2016
               expect(security_report[:month]).to eq 12
               expect(security_report[:quarter]).to eq 4
@@ -22,8 +22,8 @@ module LiteXBRL
               expect(security_report[:operating_income]).to eq '8690000000'
               expect(security_report[:ordinary_income]).to eq '8349000000'
               expect(security_report[:document_title_cover_page]).to eq '四半期報告書'
-              expect(security_report[:fiscal_year_cover_page]).to eq '第26期第２四半期(自  平成28年４月１日  至  平成28年６月30日)'
-              expect(security_report[:company_name]).to eq 'GMOインターネット株式会社'
+              expect(security_report[:fiscal_year_cover_page]).to eq '第0期第2四半期(自  平成0年0月0日  至  平成1年1月1日)'
+              expect(security_report[:company_name]).to eq 'テスト株式会社'
               expect(security_report[:filing_date]).to eq '2016-08-09'
               expect(security_report[:current_fiscal_year_start_date]).to eq '2016-01-01'
               expect(security_report[:current_fiscal_year_end_date]).to eq '2016-12-31'
