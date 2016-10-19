90fa9deea6aed22bf7b58276768ee5e076422176
YasuomiTakano <yasuomi.takano@speee.jp>
2016-10-19 17:14:12 +0900

    不要なコメントアウトを削除

--- lib/litexbrl/edinet/security_report.rb
+++ lib/litexbrl/edinet/security_report.rb
@@ -58,29 +58,7 @@ module LiteXBRL
       # 通期・四半期を取得します
       #
       def self.find_season(doc, consolidation)
-
-
-        # type_of_current_period =  doc.at_xpath("/xbrli:xbrl/jpdei_cor:TypeOfCurrentPeriodDEI")&.content
-
         doc.at_xpath("/xbrli:xbrl/jpdei_cor:TypeOfCurrentPeriodDEI")&.content
-
-        # year = doc.at_xpath("//xbrli:xbrl/xbrli:context[@id='CurrentYearInstant_#{consolidation}' or @id='CurrentYearInstant']/xbrli:entity/xbrli:identifier")
-        # quarter = doc.at_xpath("//xbrli:xbrl/xbrli:context[@id='CurrentQuarterInstant_#{consolidation}' or @id='CurrentQuarterInstant']/xbrli:entity/xbrli:identifier")
-        # q1 = doc.at_xpath("//xbrli:xbrl/xbrli:context[@id='Prior1QuarterInstant_#{consolidation}' or @id='Prior1QuarterInstant']/xbrli:entity/xbrli:identifier")
-        # q2 = doc.at_xpath("//xbrli:xbrl/xbrli:context[@id='Prior2QuarterInstant_#{consolidation}' or @id='Prior2QuarterInstant']/xbrli:entity/xbrli:identifier")
-        # q3 = doc.at_xpath("//xbrli:xbrl/xbrli:context[@id='Prior3QuarterInstant_#{consolidation}' or @id='Prior3QuarterInstant']/xbrli:entity/xbrli:identifier")
-
-        # if year
-        #   "Year"
-        # elsif quarter
-        #   "Quarter"
-        # elsif q1
-        #   "AccumulatedQ1"
-        # elsif q2
-        #   "AccumulatedQ2"
-        # elsif q3
-        #   "AccumulatedQ3"
-        # end
       end
 
       def self.find_data(doc, xbrl, context, id)
